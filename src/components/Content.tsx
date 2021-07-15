e4da6b0c0ba24561e7508b4c49d9c39cf4030f20
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-07-15 09:12:13 -0300

    finish second challenge of ReactJS

---
+++ src/components/Content.tsx
@@ -0,0 +1,28 @@
+import { MovieCard } from "./MovieCard";
+import '../styles/content.scss';
+import { MovieProps } from "../App";
+
+interface ContentProps {
+  selectedGenreTitle: string;
+  movies: MovieProps[];
+}
+
+export function Content({ selectedGenreTitle, movies }:ContentProps) {
+  
+  return (
+
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
